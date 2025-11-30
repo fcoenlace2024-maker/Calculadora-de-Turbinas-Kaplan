e8efa129058e193d7e0a41be6a1a1ae5addb8a2c
fcoenlace2024-maker <fcoenlace2024@gmail.com>
2025-11-30 00:51:44 -0600

    Create CalculadoraDeTurbinasKaplan.js

---
+++ CalculadoraDeTurbinasKaplan.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>Calculadora de Turbina Kaplan</head>
+<body>
+    <h1>Datos de diseño</h1>
+    <h2>Asigne los valores solicitados</h2>
+    <h2>Unidades: Sistema Internacional</h2>
+    let CapacidadInstalada=prompt("Capacidad Instalada En la Planta: ")
+    let Numerodeunidades=prompt("Numero de Turbinas: ")
+    let EficienciaG=prompt("Eficiencia Del Generador: ")
+    
+
+<h2>Demo JavaScript in Body</h2>
+
+<p id="demo">A Paragraph</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+</html>
